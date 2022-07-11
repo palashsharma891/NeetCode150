8f49c022e3065a48482b3fdf909901603b363564
Palash Sharma <palashsharma891@gmail.com>
2022-07-11 19:19:32 +0530

    Create 1. Binary Search.py

---
+++ Binary Search/1. Binary Search.py
@@ -0,0 +1,14 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        l, r = 0, len(nums) - 1
+        
+        while l <= r:
+            mid = (l + r) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                l = mid + 1
+            else:
+                r = mid - 1
+                
+        return -1
