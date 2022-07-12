25014b1e2cde140de4f3136e84c94e6dff5077b8
Palash Sharma <palashsharma891@gmail.com>
2022-07-12 21:53:17 +0530

    Update 7. Median of Two Sorted Arrays.py

--- Binary Search/7. Median of Two Sorted Arrays.py
+++ Binary Search/7. Median of Two Sorted Arrays.py
@@ -1,22 +1,30 @@
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        #find mid of both
-        mid1 = len(nums1) // 2
-        mid2 = len(nums2) // 2
+        A, B = nums1, nums2
+        total = len(nums1) + len(nums2)
+        half = total // 2
         
-        #avg mid1+mid2
-        s = (nums1[mid1] + nums2[mid2]) / 2
+        if len(B) < len(A):
+            A, B = B, A
         
-        #find closest number to this sum // 2
-        l, r = 0, len(nums1) + len(nums2) - 1
+        l, r = 0, len(A) - 1
+        while True:
+            i = (l + r) // 2 # A
+            j = half - i - 2 # B
         
-        while l <= r:
-            mid = (l + r) // 2
-            if mid > len(nums):
-                if nums2[mid-len(nums1)] 
-                r = len(nums) - 1
-                
-            if mid < len(nums1) and num1[mid] == s:
-                return nums[mid]
-            elif mid < len(nums2) and nums2[mid] == s:
-                return nums[mid]
+            Aleft = A[i] if i >= 0 else float("-infinity")
+            Aright = A[i + 1] if (i + 1) < len(A) else float("infinity")
+            Bleft = B[j] if j >= 0 else float("-infinity")
+            Bright = B[j + 1] if (j + 1) < len(B) else float("infinity")
+        
+            # partition is correct
+            if Aleft <= Bright and Bleft <= Aright:
+                # odd
+                if total % 2:
+                    return min(Aright, Bright)
+                # even
+                return (max(Aleft, Bleft) + min(Aright, Bright)) / 2
+            elif Aleft > Bright:
+                r = i - 1
+            else:
+                l = i + 1
