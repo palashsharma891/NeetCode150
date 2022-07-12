a0be7944aa08a8b229c2b87c0f1e4fd1ec5084d1
Palash Sharma <palashsharma891@gmail.com>
2022-07-12 18:53:30 +0530

    Create 6. Time Based Key-Value Store.py

---
+++ Binary Search/6. Time Based Key-Value Store.py
@@ -0,0 +1,29 @@
+class TimeMap:
+
+    def __init__(self):
+        self.timeMap = {}
+        
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        if key not in self.timeMap:
+            self.timeMap[key] = []
+        self.timeMap[key].append([value, timestamp])
+
+    def get(self, key: str, timestamp: int) -> str:
+        res = '' #default return value
+        values = self.timeMap.get(key, [])
+        
+        l, r = 0, len(values) - 1
+        while l <= r:
+            m = (l + r) // 2
+            if values[m][1] <= timestamp:
+                res = values[m][0]
+                l = m + 1
+            else:
+                r = m - 1
+        return res
+
+
+# Your TimeMap object will be instantiated and called as such:
+# obj = TimeMap()
+# obj.set(key,value,timestamp)
+# param_2 = obj.get(key,timestamp)
