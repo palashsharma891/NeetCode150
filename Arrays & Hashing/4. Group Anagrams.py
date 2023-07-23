219b75280abd740b0c0606ddd3d4a09c26c53f9a
Palash Sharma <palashsharma891@gmail.com>
2023-07-23 11:25:55 +0530

    added another possible solution

--- Arrays & Hashing/4. Group Anagrams.py
+++ Arrays & Hashing/4. Group Anagrams.py
@@ -2,11 +2,24 @@ class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         res = defaultdict(list)
         for s in strs:
-            
             count = [0] * 26
             for c in s:
                 count[ord(c) - ord('a')] += 1
-            
             res[tuple(count)].append(s)
-        
         return res.values()
+
+        """
+        d = {}
+        for s in strs:
+            temp = ''.join(sorted(s))
+            if temp in d:
+                d[temp] += [s]
+            else:
+                d[temp] = [s]
+
+        ans = []
+        for k in d:
+            ans += [d[k]]
+
+        return ans
+        """
