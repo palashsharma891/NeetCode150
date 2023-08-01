5a4351d9460c4fe6e8e51e5404a5314b19ccd804
Palash Sharma <palashsharma891@gmail.com>
2023-08-01 19:42:11 +0530

    Update 6. Product of Array Except Self.py

--- Arrays & Hashing/6. Product of Array Except Self.py
+++ Arrays & Hashing/6. Product of Array Except Self.py
@@ -13,3 +13,19 @@ class Solution:
             right_pro[i] *= left_pro[i]
 
         return right_pro
+
+# OR 1 array solution
+
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        ans = [1 for n in nums]
+
+        for i in range(1, len(nums)):
+            ans[i] = nums[i-1] * ans[i-1]
+
+        pro = 1
+        for i in range(len(nums)-1, -1, -1):
+            ans[i] = ans[i] * pro
+            pro *= nums[i]
+        
+        return ans
