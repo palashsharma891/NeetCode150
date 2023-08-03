bf706740afe19872d764d97e79335d35a3b6448c
Palash Sharma <palashsharma891@gmail.com>
2023-08-03 22:18:59 +0530

    Update 2. Two Sum II - Input Array Is Sorted.py

--- Two Pointers/2. Two Sum II - Input Array Is Sorted.py
+++ Two Pointers/2. Two Sum II - Input Array Is Sorted.py
@@ -1,10 +1,19 @@
-class Solution:
+zclass Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        l, r = 0, len(numbers) - 1
+        '''
+        [2,7,11,15]; 9
+         l---><---r
+        '''
+        l, r = 0, len(numbers)-1
         while l < r:
-            if numbers[l] + numbers[r] == target:
+            if  numbers[l] + numbers[r] == target:
                 return [l+1, r+1]
-            elif numbers[l] + numbers[r] < target:
-                l += 1
-            else:
+            elif numbers[l] + numbers[r] > target:
                 r -= 1
+            else:
+                l += 1
+
+        """
+        TC: O(n)
+        SC: O(1)
+        """
