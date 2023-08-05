684f3fb156dbc701c7ad52046675cb31cb2cc854
Palash Sharma <palashsharma891@gmail.com>
2023-08-05 12:01:46 +0530

    Update 3. Longest Repeating Character Replacement.py

--- Sliding Window/3. Longest Repeating Character Replacement.py
+++ Sliding Window/3. Longest Repeating Character Replacement.py
@@ -14,3 +14,27 @@ class Solution:
             res = max(res, r - l + 1)
                 
         return res
+
+# OR
+
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        l, r = 0, 0
+        count = {}
+        res = 0
+        maxf = 0
+
+        while r < len(s):
+            count[s[r]] = 1 + count.get(s[r], 0)
+            maxf = max(maxf, count[s[r]]) # maybe the newly added character is now the most frequent
+
+            while (r - l + 1) - maxf > k:
+                count[s[l]] -= 1
+                l += 1
+                # we don't have to decrement maxf because (windowlen-maxf) > k wouldn't be affected if
+                # maxf is smaller. It will only be affected if maxf increases.
+
+            res = max(res, r - l + 1)
+            r += 1
+
+        return res
