16867dad7bb5f1697d1d8a1ac36b06ebc61612ac
Palash Sharma <palashsharma891@gmail.com>
2023-08-09 20:40:31 +0530

    Update 5. Minimum Window Substring.py

--- Sliding Window/5. Minimum Window Substring.py
+++ Sliding Window/5. Minimum Window Substring.py
@@ -1,35 +1,28 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        if t == '': return ''
-        
-        t_dict, window = {}, {}
-        
-        for c in t:
-            t_dict[c] = 1 + t_dict.get(c, 0)
-            
-        have, need = 0, len(t_dict)
-        res, resLen = [-1, -1], float('inf')
+        t_map = Counter(t)
         l = 0
-        
+        res = [-1,-1]
+        resLen = float('inf')
+        have, need = 0, len(t_map)
+        window = {}
+
         for r in range(len(s)):
-            c = s[r]
-            window[c] = 1 + window.get(c, 0)
-            
-            if c in t_dict and window[c] == t_dict[c]:
+            window[s[r]] = 1 + window.get(s[r], 0)
+
+            if s[r] in t_map and window[s[r]] == t_map[s[r]]:
                 have += 1
-                
+
             while have == need:
-                #update our result
                 if r - l + 1 < resLen:
-                    resLen = r - l + 1
                     res = [l, r]
-                    
-                #pop from left of window
-                window[s[l]] -= 1
-                if s[l] in t_dict and window[s[l]] < t_dict[s[l]]:
-                    have -= 1
-                    
-                l += 1
+                    resLen = r - l + 1
                 
+                # popping from left of window
+                window[s[l]] -= 1
+                if s[l] in t_map and window[s[l]] < t_map[s[l]]:
+                    have -= 1
+                l += 1
+
         l, r = res
         return s[l:r+1]
